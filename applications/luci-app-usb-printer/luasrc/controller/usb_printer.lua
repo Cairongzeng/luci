e9954ff5336c93ac82ca9c0fa2a243b98fdf2cdc
Rongzeng Cai <cairongzeng@outlook.com>
2026-02-28 00:03:14 +0800

    Change USB printer menu location from NAS to Services

--- applications/luci-app-usb-printer/luasrc/controller/usb_printer.lua
+++ applications/luci-app-usb-printer/luasrc/controller/usb_printer.lua
@@ -21,10 +21,8 @@ function index()
 		return
 	end
 	
-  entry({"admin", "nas"}, firstchild(), "NAS", 44).dependent = false
-	
 	local page
 
-	page = entry({"admin", "nas", "usb_printer"}, cbi("usb_printer"), _("USB Printer Server"), 50)
+	page = entry({"admin", "services", "usb_printer"}, cbi("usb_printer"), _("USB Printer Server"), 50)
 	page.acl_depends = { "luci-app-usb-printer" }
 end
